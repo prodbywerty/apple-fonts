6f2f79a08f032ac020b1b26be60921f4fbf31fde
egg4096 <klopojed.son@gmail.com>
2024-01-11 23:40:30 +0200

    Overhaul

--- install.sh
+++ install.sh
@@ -1,54 +1,26 @@
 #! /bin/bash
 
-install () {
-	# $1 = Name
-	# $2 = Apple Curl Name
+font () {
+	read -N 1 -p "$1	" -s
+	echo "$REPLY"
 
-	curl https://devimages-cdn.apple.com/design/resources/download/$2.dmg -o /tmp/$1.dmg > /dev/null
-	7z x /tmp/$1.dmg "-o/tmp"
-	7z x /tmp/$1/*.pkg "-o/tmp"
-	7z x /tmp/Payload\~ "-o/tmp"
-	cp -rf /tmp/Library/Fonts/* ~/.fonts/
-	rm -rf /tmp/$1.dmg /tmp/$1 /tmp/Library /tmp/Payload~
+	if [ "$REPLY" == "y" ]
+	then
+		cp -r "$1" ~/.local/share/fonts/
+	fi
 }
 
-clear
-tput civis
+mkdir -p ~/.local/share/fonts/
 
-mkdir -p ~/.fonts
-sudo apt-get install --no-upgrade dialog curl p7zip
+echo "[y] Install"
+echo "[n]    Skip"
+echo
 
-fonts=$(dialog --checklist "Which fonts do you want to install?" 12 39 1 \
-"New York" "" on \
-"SF Arabic" "" on \
-"SF Compact" "" on \
-"SF Mono" "" on \
-"SF Pro" "" on 2>&1 >/dev/tty)
-
-clear
-tput cnorm
-
-if [[ " ${fonts[*]} " =~ " \"New York\" " ]]
-then
-	install NYFonts NY
-fi
-
-if [[ " ${fonts[*]} " =~ " \"SF Arabic\" " ]]
-then
-	install SFArabicFonts SF-Arabic
-fi
-
-if [[ " ${fonts[*]} " =~ " \"SF Compact\" " ]]
-then
-	install SFCompactFonts SF-Compact
-fi
-
-if [[ " ${fonts[*]} " =~ " \"SF Mono\" " ]]
-then
-	install SFMonoFonts SF-Mono
-fi
-
-if [[ " ${fonts[*]} " =~ " \"SF Pro\" " ]]
-then
-	install SFProFonts SF-Pro
-fi
+font "New York"
+font "San Francisco Arabic"
+font "San Francisco Armenian"
+font "San Francisco Compact"
+font "San Francisco Georgian"
+font "San Francisco Hebrew"
+font "San Francisco Mono"
+font "San Francisco Pro"
